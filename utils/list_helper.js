cef8345eb2d7ea2bec447709fed0e732e11174ff
Fatima <fcamero12@gmail.com>
2021-08-06 11:16:23 -0700

    Completed helper functions and unit tests step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  const reducer = (sum, current) => sum + current;
+  return likes.reduce(reducer)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
