cc75d2dabc79bef0ad7a120d5f497ea0aba05c39
Fatima <fcamero12@gmail.com>
2021-08-18 07:58:05 -0700

    Create users model and controller

--- models/blogs.js
+++ models/blogs.js
@@ -7,6 +7,7 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
+
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
