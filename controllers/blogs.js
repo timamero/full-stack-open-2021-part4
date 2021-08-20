f016d1ae234bc0a1e6933043685e8c9f35e752b9
Fatima <fcamero12@gmail.com>
2021-08-20 10:03:38 -0700

    Modify adding new blog so that it is only possible if a valid token is sent with the HTTP POST request

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/user')
@@ -9,9 +10,23 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const user = await User.findById(request.body.userId)
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token is missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!body.title) {
     return response.status(400).json({
